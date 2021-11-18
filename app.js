8877583e414adf77a14b61bd4ede06722914b3c4
Ashley Tran <ash@ashleys-air.myfiosgateway.com>
2021-11-18 15:21:37 -0500

    sequelized essentials

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ const express = require("express");
 const layout = require('./views/layout');
 const app = express();
 const morgan = require("morgan");
+const { db, Page, User } = require('./models');
 app.use(express.static(__dirname + "/public"))
 app.use(express.urlencoded({ extended: false}));
 
-
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
 
 app.get("/", (req, res) => {
     res.send(layout(""));
 })
 
+const init = async () => {
+    await db.sync();
+}
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`listening in post ${port}`)
 });
+
+init();
---
+++ models/index.js
@@ -0,0 +1,45 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+    logging: false
+});
+
+const Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        notEmpty: true
+      },
+      slug: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          isUrl: true,
+          notEmpty: true
+      },
+      constent: {
+          type: Sequelize.TEXT,
+          allowNull: false
+      },
+      status: {
+          type: Sequelize.ENUM('open', 'closed'),
+          allowNull: false
+      }
+});
+
+const User = db.define('user', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        notEmpty: true
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        },
+        notEmpty: true
+    }
+})
+
+module.exports = { db, Page, User };
